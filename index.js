11e38064fe2b12938b02e3190d9eab95d4f9bb5c
DerekDang05 <derekdang0801@gmail.com>
2023-11-17 04:25:35 -0500

    added index.js|Code works

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
-const inquirer= require("inquirer");
-const fs= require("fs");
+//imports for usage
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path= require("path");
 const { Square, Triangle, Circle } = require("./lib/shapes")
+
+shapeObject = {
+    Circle: Circle,
+    Triangle: Triangle,
+    Square: Square,
+};
+
+//acquires input information
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "logoName",
+            message: "Enter text for your logo [1-3 characters]: ",
+        },
+        {
+            type: "input",
+            name: "textColor",
+            message: "Enter a color for the logo's text:",
+        },
+        {
+            type: "input",
+            name: "logoColor",
+            message: "Enter a color for the logo's background:",
+        },
+
+        {
+            type: "list",
+            name: "logoShape",
+            message: "Choose the shape of your logo:",
+            choices: ["Circle", "Square",  "Triangle"],
+        },
+])
+
+
+.then((input) => {
+
+    const grabShape = shapeObject[input.logoShape];
+    const shape =  new grabShape();
+    shape.setColor(input.logoColor);
+    shape.setText(input.logoName, input.textColor);
+
+    const format = `
+    <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      <g>
+        ${shape.render()} fill="${input.logoColor}"
+        <text x="150" y="130" text-anchor="middle" font-size="28" fill="${input.textColor}">${input.logoName}</text>
+      </g>
+    </svg>`;
+
+    const examplesPath = path.join(__dirname, "examples", "logo.svg");
+
+    fs.writeFile(examplesPath, format, (err) => {
+        if (err) throw err;
+        console.log("Logo created successfully. Check examples folder");
+    });
+});
+
+
+
+
